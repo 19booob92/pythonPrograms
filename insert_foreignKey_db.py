c102dc0bacd585d8fc2ae600939b6ca0de091f14
Mateusz <19mateusz92@gmail.com>
2015-08-16 17:23:02 +0200

    UTILS SQL
    skrypt do wpisywnaia foreign key w insertach sqlowych

---
+++ insert_foreignKey_db.py
@@ -0,0 +1,27 @@
+FOREIGN_KEY_COL = 3
+
+counter = 666
+
+mergedColumn = ""
+out = []
+
+with open("example_insert.sql") as file:
+	position = 0;
+	commasCounter = 0;
+
+	content = file.read()
+
+	rows = content.split("),")
+	
+	for row in rows:
+		cols = row.split(",");
+		cols[FOREIGN_KEY_COL] = str(counter)
+		counter+=1
+		
+		mergedColumn = ",".join(cols)
+		out.append(mergedColumn)
+
+	print("),".join(out))
+					
+			
+						 	
